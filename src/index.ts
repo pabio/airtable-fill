245729c355a92ec05c5318f7534116c9a11386fc
Anand Chowdhary <mail@anandchowdhary.com>
2020-10-01 20:02:54 +0530

    :sparkles: Get all empty Airtable rows

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-export const hello = "world";
+import Airtable from "airtable";
+import { config } from "@anandchowdhary/cosmic";
+
+/** Get all rows for a table */
+export const getAllAirtableRows = <T = any>(): Promise<Array<Airtable.Record<T>>> =>
+  new Promise((resolve, reject) => {
+    const allRecords: Array<any> = [];
+    const base = new Airtable({
+      apiKey: config<string>("airtableApiKey"),
+    }).base(config<string>("airtableBase"));
+    base(config<string>("airtableTable"))
+      .select({ maxRecords: 100 })
+      .eachPage((records, fetchNextPage) => {
+        allRecords.push(...records);
+        fetchNextPage();
+      })
+      .then(() => resolve(allRecords))
+      .catch(reject);
+  });
+
+/** Get a list of all rows that have an empty column */
+export const getEmptyAirtableRows = async <T>() =>
+  (await getAllAirtableRows<T>()).filter((row) =>
+    Object.values(row.fields).find((field: any) => !field)
+  );
