cae3aa27e519da19e7f6b966b9ee7c8c02cf81ac
Anand Chowdhary <mail@anandchowdhary.com>
2020-10-02 15:57:31 +0530

    :tada: Init get data function and test

--- src/fill.ts
+++ src/fill.ts
@@ -3,3 +3,7 @@ import Airtable from "airtable";
 export const fill = async <T>(row: Airtable.Record<T>) => {
   return row.fields;
 };
+
+export const getDataFromPfister = async (url: string) => {
+  //
+};
--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,3 +1,8 @@
-test("hello world", () => {
-  expect("hello").toBe("hello");
+import { getDataFromPfister } from "./fill";
+
+test("get data from pfister", async () => {
+  const data = await getDataFromPfister(
+    "https://www.pfister.ch/de/produkt/186301/pfister-eigenmarke-sessel-winged"
+  );
+  console.log(data);
 });
