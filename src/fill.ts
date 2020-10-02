bbeaee2bf22bcea3ef1236a577f963fa5455867b
Anand Chowdhary <mail@anandchowdhary.com>
2020-10-02 16:26:29 +0530

    :construction: Fetch data from Pfister

--- src/fill.ts
+++ src/fill.ts
@@ -1,9 +1,43 @@
 import Airtable from "airtable";
+import axios from "axios";
+import { load } from "cheerio";
+
+const titleCase = (str: string) =>
+  str.replace(/\w\S*/g, (txt) => txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase());
 
 export const fill = async <T>(row: Airtable.Record<T>) => {
   return row.fields;
 };
 
 export const getDataFromPfister = async (url: string) => {
-  //
+  const { data } = await axios.get(url);
+  const $ = load(data);
+  const title = titleCase($("h1").text());
+  const prices: number[] = [];
+  $("h1 + div")
+    .find("span")
+    .each((_, price) =>
+      prices.push(
+        parseInt(
+          $(price)
+            .text()
+            .replace(/[^0-9]/g, "")
+        )
+      )
+    );
+  const [fullPrice, salePrice] = prices.sort((a, b) => b - a);
+  const description = $("h2")
+    .filter((_, elt) => $(elt).text().trim() === "Details")
+    .next()
+    .text();
+  const specifications: { [index: string]: string } = {};
+  const table = $("h2")
+    .filter((_, elt) => $(elt).text().trim() === "Details")
+    .siblings("table");
+  table.find("tr").each((_, elt) => {
+    const key = $(elt).find("th").text();
+    const value = $(elt).find("td").text();
+    specifications[key] = value;
+  });
+  return { title, fullPrice, salePrice, description, specifications };
 };
--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,8 +1,14 @@
 import { getDataFromPfister } from "./fill";
 
-test("get data from pfister", async () => {
+test("Get data from Pfister", async () => {
   const data = await getDataFromPfister(
     "https://www.pfister.ch/de/produkt/186301/pfister-eigenmarke-sessel-winged"
   );
-  console.log(data);
+  expect(data.title).toBe("Sessel Winged");
+  expect(data.fullPrice).toBe(399);
+  expect(data.description).toBe(
+    "Sessel WINGED beflügelt. Die organisch geformte Sitzfläche aus erstklassigem Rattan bietet einen hohen Sitzkomfort. Auf dem schönen Geflecht lässt sich es sich ideal entspannen und neue Energie tanken – sei es im Wohnzimmer, auf der Terrasse oder im Garten. Inklusive Sitzkissen und Nierenkissen mit abnehmbarem Bezug aus Leinen. Exklusiv bei Pfister erhältlich."
+  );
+  expect(typeof data.specifications).toBe("object");
+  expect(Object.keys(data.specifications).length).toBeGreaterThanOrEqual(1);
 });
