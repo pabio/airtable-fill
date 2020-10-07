bf3b26f65e9e39e0f853366970d284a72edcfcb7
Anand Chowdhary <mail@anandchowdhary.com>
2020-10-07 17:16:21 +0530

    :sparkles: Support multiple Airtable tables

--- src/index.ts
+++ src/index.ts
@@ -12,10 +12,10 @@ const base = new Airtable({
 }).base(config<string>("airtableBase"));
 
 /** Get all rows for a table */
-export const getAllAirtableRows = <T = any>(): Promise<Array<Airtable.Record<T>>> =>
+export const getAllAirtableRows = <T = any>(table: string): Promise<Array<Airtable.Record<T>>> =>
   new Promise((resolve, reject) => {
     const allRecords: Array<any> = [];
-    base(config<string>("airtableTable"))
+    base(table)
       .select()
       .eachPage((records, fetchNextPage) => {
         allRecords.push(...records);
@@ -26,24 +26,26 @@ export const getAllAirtableRows = <T = any>(): Promise<Array<Airtable.Record<T>>
   });
 
 /** Get a list of all rows that have an empty column */
-export const getEmptyAirtableRows = async <T>() =>
-  (await getAllAirtableRows<T>()).filter(
+export const getEmptyAirtableRows = async <T>(table: string) =>
+  (await getAllAirtableRows<T>(table)).filter(
     (row) => !(row as any).fields.Description || !("Description" in (row as any).fields)
   );
 
 /** Fill empty Airtable rows */
 export const airtableFill = async () => {
-  const rows = await getEmptyAirtableRows<Row>();
-  console.log("Got empty rows", rows.length);
-  for await (const row of rows) {
-    try {
-      const newValues = await fill<Row>(row);
-      await base(config<string>("airtableTable")).update([
-        { id: row.id, fields: { ...row.fields, ...newValues } },
-      ]);
-      console.log("Updated row", row.fields.Name);
-    } catch (error) {
-      console.log("Got an error in updating", row.id);
+  const tables = config<string>("airtableTable");
+  for await (const table of tables.split(",").map((i) => i.trim())) {
+    console.log("Starting update for table", table);
+    const rows = await getEmptyAirtableRows<Row>(table);
+    console.log("Got empty rows", rows.length);
+    for await (const row of rows) {
+      try {
+        const newValues = await fill<Row>(row);
+        await base(table).update([{ id: row.id, fields: { ...row.fields, ...newValues } }]);
+        console.log("Updated row", row.fields.Name);
+      } catch (error) {
+        console.log("Got an error in updating", row.id);
+      }
     }
   }
 };
